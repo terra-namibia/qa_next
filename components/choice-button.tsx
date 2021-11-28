98112d77061f3e6ca6298cb06c097d0a25b61c0e
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-28 11:21:46 +0900

    tailwind: styleを設定

--- components/choice-button.tsx
+++ components/choice-button.tsx
@@ -1,5 +1,9 @@
 const ChoiceButton = (props) => {
   console.log(props);
-  return <button type="button">{props.children}</button>;
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+      {props.children}
+    </button>
+  );
 };
 export default ChoiceButton;
