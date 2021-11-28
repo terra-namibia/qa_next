96c5ad45d647b3ebe54972805fb3eac10ed0fab5
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-28 16:06:06 +0900

    tailwind: add bg-yellow-100

--- components/quiz.tsx
+++ components/quiz.tsx
@@ -6,19 +6,21 @@ function quiz() {
   console.log(problems);
   return (
     <div className=" max-w-sm mr-auto ml-auto">
-      <h1 className="text-2xl md:text-4xl font-bold my-4">
-        『xxxx講座』
-        <br />
-        理解度クイズ
-      </h1>
-      <p>作成: xxxx</p>
-      <p>日本や世界に関するクイズです。</p>
-      <p>どの質問も3択です。</p>
-      <p>
-        クイズは全部で10問。
-        <strong>8問以上正解</strong>
-        できるでしょうか？
-      </p>
+      <div className="bg-yellow-100 rounded py-2 px-4 my-4">
+        <h1 className="text-2xl md:text-4xl font-bold my-4">
+          『xxxx講座』
+          <br />
+          理解度クイズ
+        </h1>
+        <p>作成: xxxx</p>
+        <p>日本や世界に関するクイズです。</p>
+        <p>どの質問も3択です。</p>
+        <p>
+          クイズは全部で10問。
+          <strong>8問以上正解</strong>
+          できるでしょうか？
+        </p>
+      </div>
       {problems.map((problem, index) => (
         <Problem key={index} problem={problem.text} index={index} />
         // console.log(`${index + 1}番目は${problem.short}です`)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Container from "../components/container";
 import Quiz from "../components/quiz";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Quiz</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Container>
         <Quiz />
       </Container>
