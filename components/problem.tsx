46c476f868c0552fc49a63596a764954d598262a
terra-namibia <tera9319@yahoo.co.jp>
2021-11-28 15:52:43 +0900

    tailwind: titleのstyle

--- components/problem.tsx
+++ components/problem.tsx
@@ -2,7 +2,7 @@ import ChoiceButton from "./choice-button";
 
 const Problem = (props) => {
   return (
-    <div className=" max-w-sm mr-auto ml-auto">
+    <>
       <h3 className="text-xl md:text-2xl font-bold tracking-tighter leading-tight md:pr-8 my-4">
         <span className="text-gray-700">質問: {props.index + 1} </span>
         {props.problem}
@@ -13,7 +13,7 @@ const Problem = (props) => {
         <ChoiceButton>cc</ChoiceButton>
         <ChoiceButton>dd</ChoiceButton>
       </div>
-    </div>
+    </>
   );
 };
 
