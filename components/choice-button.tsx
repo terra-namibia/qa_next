dae6f16ce53fae37182c44b4a0a143fc345f39b9
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-11-28 15:02:36 +0900

    tailwind: questionとchoiceのstyle

--- components/choice-button.tsx
+++ components/choice-button.tsx
@@ -1,7 +1,7 @@
 const ChoiceButton = (props) => {
   console.log(props);
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded py-2 px-4 mb-4 w-full">
       {props.children}
     </button>
   );
