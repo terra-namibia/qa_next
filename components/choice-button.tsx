6d0a1ee069a7626116f0499ed217d0e449ae7e3d
terra-namibia <tera9319@yahoo.co.jp>
2021-11-29 21:49:18 +0900

    ChoiceButtonにonClickイベントでsetAnswerする

--- components/choice-button.tsx
+++ components/choice-button.tsx
@@ -1,8 +1,19 @@
-const ChoiceButton = (props) => {
-  console.log(props);
+type Props = {
+  onClick: (index: number, answer: string) => void;
+  index: number;
+  answer: string;
+  children: string;
+};
+
+const ChoiceButton = (props: Props) => {
+  const { onClick, index, answer, children } = props;
+
   return (
-    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded py-2 px-4 mb-4 w-full">
-      {props.children}
+    <button
+      onClick={() => onClick(index, answer)}
+      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded py-2 px-4 mb-4 w-full"
+    >
+      {children}
     </button>
   );
 };
