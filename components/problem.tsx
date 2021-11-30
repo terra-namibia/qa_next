9356dafcd8c7a6b63339ce12465db69496b44e77
terra-namibia <tera9319@yahoo.co.jp>
2021-11-30 12:28:23 +0900

    ChoiceButtonのstyleやリファクタ

--- components/problem.tsx
+++ components/problem.tsx
@@ -1,31 +1,42 @@
 import ChoiceButton from "./choice-button";
+type Props = {
+  question_no: number;
+  problem: string;
+  answers: Array<string>;
+  setAnswer: (index: number, answer: string) => void;
+};
 
-const Problem = (props) => {
+const Problem = (props: Props) => {
+  const { question_no, problem, answers, setAnswer } = props;
+
   return (
     <div className="bg-indigo-50 rounded-lg py-4 px-4 my-4">
       <h3 className="text-xl md:text-2xl font-bold tracking-tighter leading-tight md:pr-8 my-4">
-        <span className="text-gray-700">質問: {props.question_no} </span>
-        {props.problem}
+        <span className="text-gray-700">質問: {question_no} </span>
+        {problem}
       </h3>
       <div className="flex flex-col">
         <ChoiceButton
-          onClick={props.setAnswer}
-          index={props.question_no}
+          setAnswer={setAnswer}
+          index={question_no}
           answer={"a"}
+          isSelected={answers[question_no] === "a"}
         >
           aa
         </ChoiceButton>
         <ChoiceButton
-          onClick={props.setAnswer}
-          index={props.question_no}
+          setAnswer={setAnswer}
+          index={question_no}
           answer={"b"}
+          isSelected={answers[question_no] === "b"}
         >
           bb
         </ChoiceButton>
         <ChoiceButton
-          onClick={props.setAnswer}
-          index={props.question_no}
+          setAnswer={setAnswer}
+          index={question_no}
           answer={"c"}
+          isSelected={answers[question_no] === "c"}
         >
           cc
         </ChoiceButton>
