026c4b71a3eb38696b99cc08e20f6bf3554d6759
terra-namibia <tera9319@yahoo.co.jp>
2021-12-21 09:28:35 +0900

    change letter-spacing and add image on Intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Container from "../components/container";
 import Quiz from "../components/quiz";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-indigo-100 text-gray-600 font-mono py-4 px-4">
+    <div className="bg-indigo-100 text-gray-600 font-mono tracking-wide	py-4 px-4">
       <Head>
         <title>Quiz</title>
       </Head>
