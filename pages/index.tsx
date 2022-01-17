7a6b3fc4c289cfd79c4edb662121d415c9258196
terra-namibia <tera9319@yahoo.co.jp>
2022-01-17 21:50:38 +0900

    add ga

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Quiz from "../components/quiz";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-indigo-100 text-gray-600 font-mono tracking-wide	py-4 px-4">
-      <Head>
-        <title>Quiz</title>
-      </Head>
       <Quiz />
     </div>
   );
