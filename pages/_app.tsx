f659fb6fbb3526dcc2c386f0605336d652af7fe9
Hirohito Terauchi <tera9319@yahoo.co.jp>
2022-01-17 21:50:38 +0900

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,54 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { GA_ID, existsGaId, pageview } from '../lib/gtag'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    if (!existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path: string) => {
+      pageview(path)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        {/* Google Analytics */}
+        {existsGaId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+              }}
+            />
+          </>
+        )}
+        <title>Quiz</title>
+      </Head>
+
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
